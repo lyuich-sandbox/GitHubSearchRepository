e843ae5d49688e803a978d04e6163cabb1349a5a
lyuich <qii3hx@gmail.com>
2017-04-10 16:35:42 +0900

    Create Repository Type:

---
+++ GitHubSearchRepository/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  GitHubSearchRepository
+//
+//  Created by lyuich on 2017/04/10.
+//  Copyright © 2017 lyuich. All rights reserved.
+//
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullName: String
+    let owner: User
+}
