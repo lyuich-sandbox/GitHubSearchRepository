7c9f399e68caf23e23c8168118fc993c7efce645
lyuich <qii3hx@gmail.com>
2017-04-10 16:35:30 +0900

    Create User type

---
+++ GitHubSearchRepository/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  GitHubSearchRepository
+//
+//  Created by lyuich on 2017/04/10.
+//  Copyright © 2017 lyuich. All rights reserved.
+//
+
+struct User {
+    let id: Int
+    let login: String
+}
