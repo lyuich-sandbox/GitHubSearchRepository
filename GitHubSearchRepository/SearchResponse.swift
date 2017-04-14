e8fb35b98c136ccfeabc8db1bd6f13aceb362d61
lyuich <qii3hx@gmail.com>
2017-04-14 23:42:47 +0900

    Initialize SearchResponse

--- GitHubSearchRepository/SearchResponse.swift
+++ GitHubSearchRepository/SearchResponse.swift
@@ -6,7 +6,28 @@
 //  Copyright © 2017 lyuich. All rights reserved.
 //
 
-struct SearchResponse<Item> {
+struct SearchResponse<Item: JSONDecodable> : JSONDecodable {
     let totalCount: Int
     let items: [Item]
+
+    init(json: Any) throws {
+        guard let dictionary = json as? [String : Any] else {
+            throw JSONDecodeErorr.invalidFormat(json: json)
+        }
+
+        guard let totalCount = dictionary["total_count"] as? Int else {
+            throw JSONDecodeErorr.missingValue(key: "total_count", actualValue: dictionary["total_count"])
+        }
+
+        guard let itemObjects = dictionary["items"] as? [Any] else {
+            throw JSONDecodeErorr.missingValue(key: "items", actualValue: dictionary["items"])
+        }
+
+        let items = try itemObjects.map {
+            return try Item(json: $0)
+        }
+
+        self.totalCount = totalCount
+        self.items = items
+    }
 }
