499e3409f6fe7bea3671438aae4ca1f1db6d4b3f
lyuich <qii3hx@gmail.com>
2017-04-14 22:27:26 +0900

    Initialize User

--- GitHubSearchRepository/User.swift
+++ GitHubSearchRepository/User.swift
@@ -9,4 +9,21 @@
 struct User {
     let id: Int
     let login: String
+
+    init(json: Any) throws{
+        guard let dictionary = json as? [String : Any] else {
+            throw JSONDecodeErorr.invalidFormat(json: json)
+        }
+
+        guard let id = dictionary["id"] as? Int else {
+            throw JSONDecodeErorr.missingValue(key: "id", actualValue: dictionary["id"])
+        }
+
+        guard let login = dictionary["login"] as? String else {
+            throw JSONDecodeErorr.missingValue(key: "login", actualValue: dictionary["login"])
+        }
+
+        self.id = id
+        self.login = login
+    }
 }
