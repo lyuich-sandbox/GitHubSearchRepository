2f5feb097d06f24abe7e7f664445a51c74c3dda4
lyuich <qii3hx@gmail.com>
2017-04-15 14:01:36 +0900

    Initialize GitHubAPIError.FieldError

--- GitHubSearchRepository/GitHubAPIError.swift
+++ GitHubSearchRepository/GitHubAPIError.swift
@@ -7,6 +7,34 @@
 //
 
 struct GitHubAPIError : JSONDecodable, Error {
+    struct FieldError : JSONDecodable {
+        let resource: String
+        let field: String
+        let code: String
+
+        init(json: Any) throws {
+            guard let dictionary = json as? [String : Any] else {
+                throw JSONDecodeError.invalidFormat(json: json)
+            }
+
+            guard let resource = dictionary["resource"] as? String else {
+                throw JSONDecodeError.missingValue(key: "resource", actualValue: dictionary["resource"])
+            }
+
+            guard let field = dictionary["field"] as? String else {
+                throw JSONDecodeError.missingValue(key: "field", actualValue: dictionary["field"])
+            }
+
+            guard let code = dictionary["code"] as? String else {
+                throw JSONDecodeError.missingValue(key: "code", actualValue: dictionary["code"])
+            }
+
+            self.resource = resource
+            self.field = field
+            self.code = code
+        }
+    }
+
     let message: String
 
     init(json: Any) throws {
