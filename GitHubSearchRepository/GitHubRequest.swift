6a88bb398b995124bac4e67402cc22dbbaa78939
lyuich <qii3hx@gmail.com>
2017-04-15 15:42:39 +0900

    Define the protocol of GitHubRequest

---
+++ GitHubSearchRepository/GitHubRequest.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubRequest.swift
+//  GitHubSearchRepository
+//
+//  Created by lyuich on 4/15/17.
+//  Copyright © 2017 lyuich. All rights reserved.
+//
+
+import Foundation
+
+protocol GitHubRequest {
+    var baseURL: URL { get }
+    var path: String { get }
+}
+
+extension GitHubRequest {
+    var baseURL: URL {
+        return URL(string: "https://api.github.com")!
+    }
+}
