cbb96d9013ef368c2b5168a5338ba70d4c4ec44b
lyuich <qii3hx@gmail.com>
2017-04-17 19:23:18 +0900

    Add a case of .connectionError

--- GitHubSearchRepository/GitHubClient.swift
+++ GitHubSearchRepository/GitHubClient.swift
@@ -15,11 +15,15 @@ class GitHubClient {
         return session
     }()
 
-    func send<Request : GitHubRequest>(request: Request, completion: (Result<Request.Response, GitHubClientError>) -> Void) {
+    func send<Request : GitHubRequest>(request: Request, completion: @escaping (Result<Request.Response, GitHubClientError>) -> Void) {
         let urlRequest = request.buildURLRequest()
         let task = session.dataTask(with: urlRequest) {
             data, response, error in
 
+            switch (data, response, error) {
+            case (_, _, let error?):
+                completion(Result(error: .connectionError(error)))
+            }
         }
         
         task.resume()
